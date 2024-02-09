4269af2bd8e89b6abdd8a86f42c883eb4a8bc1c9
Henry Zhao <zhaohenry2006@gmail.com>
2024-02-08 17:35:58 -0800

    a

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from Student import Student
 courseList = {"English":["AP Lit", "Eng 12"], "Math":["Multivar", "Calc BC", "Calc AB", "Precalc"], 
            "Social Science":["Gov/Econ"], "Art:":["Band", "Orchestra", "Digital Imaging", "Ceramics"], 
            "Electives":["AP Physics 1", "AP Physics C", "AP Stats", "AP Chem", "AP Bio", "AP Human Geo", "APES", "TA"] }
@@ -6,14 +7,39 @@ difficulty = {"AP Lit":1.05, "Eng 12":0.95,
                   "Multivar":1.05, "Calc BC":1.05, "Calc AB":1.02, "Precalc":0.98,
                   "Gov/Econ":1, "Band": 0.97, "Orchestra":0.97, "Digital Imaging":0.97, "Ceramics":0.97,
                   "AP Physics 1":1.03, "AP Physics C":1.05, "AP Stats":1.02, "AP Chem":1.03, "AP Bio":1.03, "AP Human Geo":1.03, "APES":1.03, "TA":0.97}
+extracurriculars = {"Underwater Tennis":5, "Picket Fence Long Jump":3, "Blindfold Lacrosse":5, "Sky-high Badminton":4, "Underwater Basket Weaving":0}
+ECList = ["Underwater Tennis", "Picket Fence Long Jump", "Blindfold Lacrosse", "Sky-high Badminton", "Underwater Basket Weaving"]
+
 def main():
     print("Welcome to MSJ Life Simulator! You a are a current student at the beginning of their senior year.\nWhile you currently have a very clingy girlfriend, you are trying to have a social life while still keeping up with grades and college apps. ")
     print("\nSelect your courses.")
     schedule = chooseCourses(courseList)
-    student = student(schedule)
+    ec = chooseEC()
+    student = Student(schedule, ec)
 
     while student.week <= 36:
-        print("Week: " + student.week + "Current GPA: " + student.GPA)
+        print("Week: " + str(student.week) + " | Current GPA: " + str(student.GPA))
+        student.doWeek()
+
+
+
+def chooseNum(nums):
+    validSelection = False
+    while not validSelection:
+        sel = input()
+        if sel.isdigit() and 1 <= int(sel) <= nums:
+            print()
+            return int(sel)
+        else:
+            print("Invalid input. Try again.")
+
+def chooseEC():
+    options = "Choose your extracurricular by picking ONE from the following list: \n"
+    for i in range(0, len(ECList)):
+        options += str(i + 1) + ". " + ECList[i] + " | "
+    options = options[0:-3]
+    print(options)
+    return chooseNum(5)
 
 
 def chooseCourses(courseList):
@@ -24,7 +50,7 @@ def chooseCourses(courseList):
             for i in range(1, len(courseList[item]) + 1):
                 options += str(i) + ". " + courseList[item][i-1] + " | "
             options = options[0:-3]
-            print(options + "\n")
+            print(options)
             
             validSelection = False
             while not validSelection:
@@ -42,7 +68,6 @@ def chooseCourses(courseList):
             print(options + "\n")
             
             validSelection = False
-            selectedCourse = 0
             while not validSelection:
                 sel = input()
                 if len(sel) >= 3:
@@ -52,6 +77,7 @@ def chooseCourses(courseList):
                         finalCourses.append(courseList[item][int(first) - 1])
                         finalCourses.append(courseList[item][int(second) - 1])
                         validSelection = True
+                        print()
                 else:
                     print("Invalid input. Try again.")
     return finalCourses
--- student.py
+++ student.py
@@ -1,13 +1,14 @@
-class student:
-    def __init__(self, sched):
+class Student:
+    def __init__(self, schedule, ec):
         self.GPA = 4.0
-        self.schedule = sched
+        self.schedule = schedule
+        self.ec = ec
         self.week = 1
+        self.quarter = 1
         self.relationship = True
 
         self.luck = 1
-        self.loveScore = 100
+        self.loveScore = 75
         
-
-
-    
+    def doWeek():
+        pass
